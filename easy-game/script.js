d25471be6ddd5b30d83dce66c65c91c99dbc9414
tramanh <tramanh@bnk.com>
2025-06-22 23:27:15 +0700

    update pass

--- easy-game/script.js
+++ easy-game/script.js
@@ -1,6 +1,6 @@
 function checkPassword() {
   const input = document.getElementById("password").value;
-  const correctPassword = "matkhau123"; // <-- bạn đổi ở đây
+  const correctPassword = "010298TramAnhCute"; // <-- bạn đổi ở đây
 
   if (input === correctPassword) {
     window.location.href = "raffle.html";
